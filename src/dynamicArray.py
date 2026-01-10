6fb74b4f46502d09b692c0994424b861db002ba5
Gio <j091670s@pacificu.edu>
2026-01-09 17:12:18 -0800

    trying to figure out why my print array functyion prints 3 times when i appended twice

---
+++ src/dynamicArray.py
@@ -0,0 +1,35 @@
+#! myenv\Scripts\python.exe
+
+import numpy as np 
+
+class dynamicArray:
+    # variables for sizing
+    CAPACITY = 15
+    SIZE = 0   
+    
+    #create the dynamic array
+    def __init__(self,capacity = 15):
+        self.size = 0
+        self.capacity = capacity
+        self.data = [None] * capacity
+    
+    # print the array
+    def print_dArray(self):
+        for item in range(self.data):
+            print(self.data)
+            
+    # method to return the size     
+    def give_Size(self):
+        return self.size
+    
+    def append_Element (self, element_1):
+        current_Size = self.give_Size()
+        
+        if(current_Size > self.capacity):
+            self.capacity += 1
+            self.data[-1] = element_1
+        else :
+            self.data = element_1
+            self.size += 1
+            
+            
