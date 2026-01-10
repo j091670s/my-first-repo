e0acbbdf6b6b81141631141c81bd3b0880ea75df
Gio <j091670s@pacificu.edu>
2026-01-09 19:11:51 -0800

    debug the resizing

--- src/dynamicArray.py
+++ src/dynamicArray.py
@@ -12,31 +12,37 @@ class dynamicArray:
         self.size = 0
         self.capacity = capacity
         self.data = [None] * capacity
-    
-   
-            
+    #################################################################################################
+
     # method to return the size     
     def give_Size(self):
         return self.size
+    #################################################################################################
     
-     # print the array
+    
+    # print the array
     def print_dArray(self):
         print(self.data)
+    #################################################################################################
     
+    # append element to the end of the array
     def append_Element (self, element_1):
         current_Size = self.give_Size()
         
-        if(current_Size > self.capacity):
-            print("The current size:", current_Size, " exceeds the capacity:", self.capacity, ".Resizing by one\n")
-            self.capacity += 1
-            self.data[-1] = element_1
-            self.size += 1
-            print ("New size is: ", self.capacity)
+        if(current_Size >= self.capacity):
+            print("\nThe current size:", current_Size, " exceeds the capacity:", self.capacity, ".Resizing by one")
+            self.capacity +=1
+            self.size+=1
+            new_Array = self
+            new_Array.size = self.size
+            new_Array.data = self.data[:]
+            new_Array.data[-1] = element_1
+            print ("New size is: ", self.size)
         else :
             print("\nCurrent size:", self.size,". Current capacity:", self.capacity)
             self.data[current_Size] = element_1
             self.size += 1
             print("New size:", self.size,". Current capacity:", self.capacity)
 
-            
+    #################################################################################################
             
--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ from dynamicArray import dynamicArray
 
 my_dynamic_Array = dynamicArray(5)
 
-my_dynamic_Array.append_Element(1)
+for i in range (5):
+    my_dynamic_Array.append_Element(1)
+
 
 my_dynamic_Array.print_dArray()
-
 my_dynamic_Array.append_Element(2)
-
 my_dynamic_Array.print_dArray()
+
