05341c56f202f2953d53c9327fd338fe0c8cd36e
Gio <j091670s@pacificu.edu>
2026-01-09 18:39:49 -0800

    added debug output to track the size of the dynamic array

--- src/dynamicArray.py
+++ src/dynamicArray.py
@@ -13,23 +13,30 @@ class dynamicArray:
         self.capacity = capacity
         self.data = [None] * capacity
     
-    # print the array
-    def print_dArray(self):
-        for item in range(self.data):
-            print(self.data)
+   
             
     # method to return the size     
     def give_Size(self):
         return self.size
     
+     # print the array
+    def print_dArray(self):
+        print(self.data)
+    
     def append_Element (self, element_1):
         current_Size = self.give_Size()
         
         if(current_Size > self.capacity):
+            print("The current size:", current_Size, " exceeds the capacity:", self.capacity, ".Resizing by one\n")
             self.capacity += 1
             self.data[-1] = element_1
-        else :
-            self.data[self.size] = element_1
             self.size += 1
+            print ("New size is: ", self.capacity)
+        else :
+            print("\nCurrent size:", self.size,". Current capacity:", self.capacity)
+            self.data[current_Size] = element_1
+            self.size += 1
+            print("New size:", self.size,". Current capacity:", self.capacity)
+
             
             
--- src/main.py
+++ src/main.py
@@ -2,7 +2,11 @@ import dynamicArray
 from dynamicArray import dynamicArray
 
 my_dynamic_Array = dynamicArray(5)
+
 my_dynamic_Array.append_Element(1)
+
 my_dynamic_Array.print_dArray()
+
 my_dynamic_Array.append_Element(2)
+
 my_dynamic_Array.print_dArray()
